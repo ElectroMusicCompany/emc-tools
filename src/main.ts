7dae3c6e8f61b4455a6dc041d1a1c474dfe500cf
opera7133 <ainznino@pm.me>
2024-03-18 00:50:48 +0900

    comment

--- src/main.ts
+++ src/main.ts
@@ -4,14 +4,17 @@ import { SpotifyApi, AccessToken } from '@spotify/web-api-ts-sdk';
 import { parse } from 'node-html-parser';
 import Fastify from 'fastify';
 
+// .envの読み取り
 dotenv.config();
 
+// 初期化用
 const client = new Client({ intents: ['Guilds', 'GuildMessages', 'MessageContent', 'GuildMembers'] });
 
 const fastify = Fastify();
 
 let spotify: SpotifyApi;
 
+// SongWhipのレスポンス
 interface SongWhip {
   type: 'album' | 'track' | 'playlist';
   id: string;
@@ -41,12 +44,15 @@ interface SongWhip {
   url: string;
 }
 
+// 適当なエンドポイント
 fastify.get('/', async (request, reply) => {
   return { hello: 'world' };
 });
 
+// Spotifyのコールバック用エンドポイント
 fastify.get('/callback', async (request, reply) => {
   const { code } = request.query as { code: string };
+  // トークンの取得
   const res = await fetch('https://accounts.spotify.com/api/token', {
     method: 'POST',
     headers: {
@@ -65,10 +71,12 @@ fastify.get('/callback', async (request, reply) => {
   delete data.scope;
   data.expires = data.expires_in * 1000 + Date.now();
   const spotifyToken = data as AccessToken;
+  // SpotifyのAPIの初期化
   spotify = SpotifyApi.withAccessToken(process.env.SPOTIFY_CLIENT_ID || '', spotifyToken);
   return { status: 'OK' };
 });
 
+// サーバーの起動
 fastify.listen({ port: 3000 }, (err, address) => {
   if (err) {
     console.error(err);
@@ -77,6 +85,7 @@ fastify.listen({ port: 3000 }, (err, address) => {
   console.log(`Server listening on ${address}`);
 });
 
+// Discord Bot起動時の処理
 client.once('ready', async () => {
   console.log('Ready!');
   console.log(client.user?.tag);
@@ -85,16 +94,21 @@ client.once('ready', async () => {
   );
 });
 
+// スラッシュコマンドの処理
 client.on('interactionCreate', async (interaction: Interaction) => {
   if (!interaction.isCommand()) {
     return;
   }
   const { commandName, options } = interaction;
+  // 反応確認
   if (commandName === 'ping') {
     await interaction.reply('Pong!');
   }
+  // Spotifyの認証
   if (commandName === 'auth_spotify') {
+    // URLがあれば、トークンを取得
     if (options.get('url')?.value !== null) {
+      // 中身は上と一緒
       const url = new URLSearchParams(options.get('url')!.value as string);
       const code = url.get('code');
       const res = await fetch('https://accounts.spotify.com/api/token', {
@@ -118,25 +132,29 @@ client.on('interactionCreate', async (interaction: Interaction) => {
       spotify = SpotifyApi.withAccessToken(process.env.SPOTIFY_CLIENT_ID || '', spotifyToken);
       await interaction.reply({ content: 'Authenticated with Spotify!', ephemeral: true });
     } else {
+      // URLがなければ、URLを返す
       await interaction.reply({
         content: `https://accounts.spotify.com/authorize?client_id=${process.env.SPOTIFY_CLIENT_ID}&response_type=code&redirect_uri=${process.env.SPOTIFY_REDIRECT_URI}&scope=playlist-modify-public%20playlist-modify-private`,
         ephemeral: true,
       });
       await interaction.followUp({
-        content: 'Please enter the code you received from Spotify.\n`/auth_spotify code:YOUR_URL',
+        content: 'SpotifyのコールバックURLをコマンドに入れてください。\n`/auth_spotify url:YOUR_URL',
         ephemeral: true,
       });
     }
   }
 });
 
+// メッセージの処理
 client.on('messageCreate', async (message) => {
   if (message.author.bot) return;
+  // 正規表現で音楽サブスクURLを抽出
   const musicRegex =
     /(?:http:\/\/|https:\/\/)?(?:[a-z0-9]*[\-\.])*(?:apple|spotify|youtube|youtu|bandcamp|tidal|pandora|napster|yandex|amazon|deezer|jiosaavn|audius|gaana|soundcloud|page)\.(?:com|co|link|be)(?:\/[^ |\n|\t|\"|\']*)+/;
 
   if (musicRegex.test(message.content)) {
     const musicUrl = message.content.match(musicRegex)![0];
+    // SongWhipにURLを投げて、SpotifyのURLを取得
     const res = await fetch(`https://songwhip.com`, {
       method: 'POST',
       body: JSON.stringify({ url: musicUrl }),
@@ -145,15 +163,19 @@ client.on('messageCreate', async (message) => {
       },
     });
     const data: SongWhip = await res.json();
+    // SpotifyのURLがあれば、Spotifyのプレイリストに追加
     if (data.links.spotify) {
+      // SongWhipのページをスクレイピングして、SpotifyのURLを取得
       const servicesRes = await fetch(data.url);
       const servicesData = parse(await servicesRes.text());
-      // Find tags containing the word Spotify in <a> <div>.
+      // <a>タグの中からSpotifyのURLを取得
       const spotifyTags = Array.from(servicesData.querySelectorAll('a')).filter((tag) =>
         tag.text.includes('Spotify'),
       )[0];
       const spotifyId = spotifyTags.getAttribute('href')!.split('/').pop() || '';
+      // SpotifyのAPIを使って、曲の情報を取得
       const songData = await spotify.tracks.get(spotifyId);
+      // プレイリストに追加
       await spotify.playlists.addItemsToPlaylist(process.env.SPOTIFY_PLAYLIST_ID || '', [songData.uri]);
     }
     message.reply(data.url);
